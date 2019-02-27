221259564bcdae126625009082c3486e14a5eca5
eduar <bartenderprov1@gmail.com>
2019-02-27 19:36:10 -0300

    layout recuperar senha pronto

--- app/src/main/java/bartenderpro/fragment/RecuperarSenha.java
+++ app/src/main/java/bartenderpro/fragment/RecuperarSenha.java
@@ -1,4 +1,4 @@
-package com.example.eduar.bartenderpro;
+package bartenderpro.fragment;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.eduar.bartenderpro.R;
 
 
 /**
