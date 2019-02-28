375f98ed458134a1c4f27690648c98b768f0bc55
Leonardo <bartenderprov1@gmail.com>
2019-02-28 19:41:46 -0300

    commit de teste

--- app/src/main/java/bartenderpro/fragment/RecuperarSenha.java
+++ app/src/main/java/bartenderpro/fragment/RecuperarSenha.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,6 +67,7 @@ public class RecuperarSenha extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.d("teste", "teste");
         return inflater.inflate(R.layout.fragment_recuperar_senha, container, false);
     }
 
